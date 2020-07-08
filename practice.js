2ebbf47b91e9c02c4c7657ae1e3f8f5d7438950e
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-08 16:40:29 -0600

    made some head way stuck 7 and on

--- practice.js
+++ practice.js
@@ -24,14 +24,14 @@ let lovesCode = true;
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
-let greatestFear =
+let greatestFear 
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
-let(devMountainGoal) = null
+let devMountainGoal = null
 //////////////////PROBLEM 6////////////////////
 
 //Create a function declaration called greeting that
@@ -50,10 +50,6 @@ greeting(Kody)
 //Give it the same functionality as the function greeting in Problem 6.
 
 //Code Here
-function newGreeting(name){
-  return "Hello, " + name
-}
-greeting(Kody)
 
 //////////////////PROBLEM 8////////////////////
 
@@ -61,7 +57,7 @@ greeting(Kody)
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
-Array["apples", "milk", "eggs", "bread"];
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 
 //////////////////PROBLEM 9////////////////////
@@ -72,11 +68,20 @@ Array["apples", "milk", "eggs", "bread"];
 //and goodBoy (a boolean).
 
 //Code Here
+let dog = {
+  name: 'Luna',
+  color: 'Tan',
+  age: 6,
+  goodBoy: true,
 
+}
+  
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
+
+dog.name = devMountainClassPet
 
 //////////////////PROBLEM 10////////////////////
 
@@ -88,6 +93,13 @@ Array["apples", "milk", "eggs", "bread"];
 // with NAMEPARAM being the name parameter being passed in (not literally NAMEPARAM)
 
 // Code here
+function nameCheck(name) {
+  if (name === "Steven") {
+    
+  }
+  return "What is up Steven"
+}
+nameCheck(Steven)
 
 //////////////////PROBLEM 11////////////////////
 
@@ -97,11 +109,15 @@ Array["apples", "milk", "eggs", "bread"];
 
 //Code Here
 
+function add(num1, num2) {
+  return num1 + num2
+}
+
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
 //Code Here
-
+let mathSum = add(3,4)
 //////////////////PROBLEM 12////////////////////
 
 //Write a function called faveColorFinder that takes in one parameter called color
