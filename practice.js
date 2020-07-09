6648fd1b9188a53eede60581ee6a022d0067ce28
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-08 19:06:01 -0600

    problem 14 done

--- practice.js
+++ practice.js
@@ -185,12 +185,13 @@ let pondScope = ["duck", "realDuck"];
 //Create a variable called age with your age assigned to you
 
 // Code Here
-
+let age = 26
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
 // Code Here
-
+age++
 // Good news! We can live forever. Set your age to 999
 
 // Code Here
+age = 999
